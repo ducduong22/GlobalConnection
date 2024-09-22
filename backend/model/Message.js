fb4b768faeb10bb70c38c1ee6a596b873f31d6c2
ducduong22 <ducduong387@gmail.com>
2024-09-22 17:41:29 +0700

    SiocalProject

---
+++ backend/model/Message.js
@@ -0,0 +1,18 @@
+// server/model/Message.js
+const mongoose = require("mongoose");
+
+const messageSchema = new mongoose.Schema(
+  {
+    senderId: { type: String, required: true },
+    receiverId: {
+      type: String,
+      required: true,
+    },
+    content: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+const Message = mongoose.model("Message", messageSchema);
+
+module.exports = Message;
